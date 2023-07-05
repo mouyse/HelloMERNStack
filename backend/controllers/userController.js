4e750e0c3d7632dd870e9648fc84cf51d7a841e2
Jay <jayy.shah16@gmail.com>
2023-07-05 16:19:42 +0530

    Signing Up & Hashing Passwords implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 // Login user
 const loginUser = async (req, res) => {
 
-    res.json({msg: 'Login user'})
+    res.json({mssg: 'Login user'})
 }
 
 
@@ -12,7 +12,16 @@ const loginUser = async (req, res) => {
 // Login user
 const signUpUser = async (req, res) => {
 
-    res.json({msg: 'Sign up user'})
+    const { email, password } = req.body
+
+    try{
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
+
+    res.json({mssg: 'Sign up user'})
 }
 
 module.exports = { signUpUser, loginUser }
